4d32eee6862aba6ac67d4b334707a6de447301b9
aakashkardam <aakashkardam94@gmail.com>
2019-02-19 05:42:46 +0000

    images successfully loaded

--- model.py
+++ model.py
@@ -6,19 +6,26 @@ from keras.layers import Flatten, Dense
 
 ### Read the data from the csv file
 lines = []
-with open('/home/workspace/CarND-Behavioral-Cloning-P3/data/driving_log.csv') as csvfile:
+#with open('/home/workspace/CarND-Behavioral-Cloning-P3/data/driving_log.csv') as csvfile:
+with open('../../../../opt/carnd_p3/data/driving_log.csv') as csvfile:
   render = csv.reader(csvfile)
   for line in render:
     lines.append(line)
     #print(line[0],"" ,line[3]) 
   
 
-print(len(lines))
+#print(len(lines))
+#print(lines[0])
 
 images = [] #----------------------------- stores the training images (features)                 
 measurements = [] #----------------------- stores the steering angle (label)
 for line in lines:
-  image = cv2.imread(line[0])
+  source_path=line[0]
+  #print(source_path)
+  filename = source_path.split('/')[-1]
+  current_path = '../data/IMG/'+filename
+  image = cv2.imread(current_path)
+  #print(image)
   images.append(image)
   measurement = float(line[3])
   measurements.append(measurement)
@@ -26,10 +33,11 @@ for line in lines:
 ### Training the network
 X_train = np.array(images) #-------------- converts the image data into a numpy array 
 y_train = np.array(measurements) #-------- converts the steering data into a numpy array
-
+#print(X_train)
+#print(X_train.shape)
 model = Sequential()
-model.add(Flatten(input_shape=(len(lines),640,480,3)))
+model.add(Flatten(input_shape=(160,320,3)))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train,y_train,validation_split=0.2,shuffle=True)
-model.save('model_regression.h5')
+model.save('model_regression_Udacity_data.h5')
