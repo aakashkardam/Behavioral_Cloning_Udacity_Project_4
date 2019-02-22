21108c05fed53eabb8e9bad66c9ce01e6bc58658
aakashkardam <aakashkardam94@gmail.com>
2019-02-22 04:08:26 +0000

    Conv 2D layers added, needs further improvement

--- model.py
+++ model.py
@@ -2,12 +2,12 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Conv2D
 
 ### Read the data from the csv file
 lines = []
-#with open('/home/workspace/CarND-Behavioral-Cloning-P3/data_new/driving_log.csv') as csvfile:
-with open('data/driving_log.csv') as csvfile:
+with open('/home/workspace/CarND-Behavioral-Cloning-P3/data_new/driving_log.csv') as csvfile:
+#with open('data_1/driving_log.csv') as csvfile:
   render = csv.reader(csvfile)
   for line in render:
     lines.append(line)
@@ -23,7 +23,7 @@ for line in lines:
   source_path=line[0]
   #print(source_path)
   filename = source_path.split('/')[-1]
-  current_path = '../data_1/IMG/'+filename
+  current_path = '../data/IMG/'+filename
   image = cv2.imread(current_path)
   #print(image)
   images.append(image)
@@ -36,8 +36,17 @@ y_train = np.array(measurements) #-------- converts the steering data into a num
 #print(X_train)
 #print(X_train.shape)
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x/255 - 0.5, input_shape=(160,320,3)))
+model.add(Conv2D(24, 5, 5, activation='relu', subsample=(2, 2)))
+model.add(Conv2D(36, 5, 5, activation='relu', subsample=(2, 2)))
+model.add(Conv2D(48, 5, 5, activation='relu', subsample=(2, 2)))
+model.add(Conv2D(64, 3, 3, activation='relu'))
+model.add(Conv2D(64, 3, 3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train,y_train,validation_split=0.2,shuffle=True)
+model.fit(X_train,y_train,validation_split=0.2,shuffle=True,epochs=10)
 model.save('model_regression_Udacity_data.h5')
