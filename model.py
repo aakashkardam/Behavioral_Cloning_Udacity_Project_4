3d82f8460c5f825bc8cc7ff60201716dd41fedd8
aakashkardam <aakashkardam94@gmail.com>
2019-03-04 16:35:21 -0500

    Formatted the code and add comments

--- model.py
+++ model.py
@@ -5,9 +5,8 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D
 import matplotlib.pyplot as plt
 
-### Read the data from the csv file
+#------------------------------------------------ Read the data from the csv file
 lines = []
-#with open('/home/workspace/CarND-Behavioral-Cloning-P3/data_new/driving_log.csv') as csvfile:
 with open('/home/workspace/CarND-Behavioral-Cloning-P3/data_new/driving_log.csv') as csvfile:#use this in GPU mode
 #with open('/home/workspace/CarND-Behavioral-Cloning-P3/data_1/driving_log.csv') as csvfile:# use this in CPU mode
   next(csvfile)
@@ -22,55 +21,51 @@ with open('/home/workspace/CarND-Behavioral-Cloning-P3/Additional_Data_turns/dri
     lines1.append(line)
  
   
-
+#------------------------------------------------ Debugging statements
 #print(len(lines))
 #print(lines[0])
 
-images = [] #----------------------------- stores the training images (features)                 
-measurements = [] #----------------------- stores the steering angle (label)
+
+images = []                                     #----------------------------- stores the training images (features)                 
+measurements = []                               #----------------------- stores the steering angle (label)
 for line in lines:
-  for i in range(3): # using multiple cameras. try this later
+  for i in range(3):                            # using multiple cameras
     source_path=line[i]
     #print(source_path)
     filename = source_path.split('/')[-1]
-    current_path = 'data_new/IMG/'+filename # use this in GPU mode
-    #current_path = 'data_1/IMG/'+filename # use this in CPU mode
+    current_path = 'data_new/IMG/'+filename     # use this in GPU mode
+    #current_path = 'data_1/IMG/'+filename      # use this in CPU mode
     image = cv2.imread(current_path)
-    #print(image)
     images.append(image)
     measurement = float(line[3])
     if (i==1):
-      measurements.append(measurement + 0.05)
+      measurements.append(measurement + 0.05)   # correcting for steering from the left camera image
     elif (i==2):
-      measurements.append(measurement - 0.05)
+      measurements.append(measurement - 0.05)   # correcting for steering from the right camera image
     else:
       measurements.append(measurement)
 
 
 for line in lines1:
-  for i in range(3): # using multiple cameras. try this later
+  for i in range(3):                            # using multiple cameras from the additional data
     source_path=line[i]
     #print(source_path)
     filename = source_path.split('/')[-1]
-    current_path = 'Additional_Data_turns/IMG/'+filename # use this in GPU mode
-    #current_path = 'data_1/IMG/'+filename # use this in CPU mode
+    current_path = 'Additional_Data_turns/IMG/'+filename     # use this in GPU mode
+    #current_path = 'data_1/IMG/'+filename                   # use this in CPU mode
     image = cv2.imread(current_path)
     #print(image)
     images.append(image)
     measurement = float(line[3])
     if (i==1):
-      measurements.append(measurement + 0.05)
+      measurements.append(measurement + 0.05)   # correcting for steering from the left camera image
     elif (i==2):
-      measurements.append(measurement - 0.05)
+      measurements.append(measurement - 0.05)   # correcting for steering from the right camera image
     else:
        measurements.append(measurement)
 
 
-
-
-
-
-### Data AUgmentation : flip images
+#---------------------------------------------- Data Augmentation : flip images
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
   augmented_images.append(image)
@@ -78,13 +73,11 @@ for image, measurement in zip(images, measurements):
   augmented_images.append(cv2.flip(image,1))
   augmented_measurements.append(measurement*-1.0)
 
-### Training the network
-X_train = np.array(augmented_images) #-------------- converts the image data into a numpy array 
-y_train = np.array(augmented_measurements) #-------- converts the steering data into a numpy array
-#print(X_train)
-#print(X_train.shape)
-#plt.hist(y_train)
-#plt.show()
+#---------------------------------------------- Collecting the training data
+X_train = np.array(augmented_images)            #-------------- converts the image data into a numpy array 
+y_train = np.array(augmented_measurements)      #-------------- converts the steering data into a numpy array
+
+#---------------------------------------------- Building the Neural Netwrok
 model = Sequential()
 model.add(Lambda(lambda x: x/255 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
@@ -99,5 +92,9 @@ model.add(Dense(50, activation='relu'))
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
+
+#--------------------------------------------- Training the Neural Network
 model.fit(X_train,y_train,validation_split=0.2,shuffle=True,epochs=5)
+
+#--------------------------------------------- Saving the trained Neural Network
 model.save('model_regression_Udacity_data5.h5')
